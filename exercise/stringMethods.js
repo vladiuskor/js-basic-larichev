6541768822d805d0283d4eff6ce6205287a7c268
Vlad Korobko <max.vlad.kor@gmail.com>
2023-01-10 20:09:48 +0200

    Added string methods second exercise solution

--- exercise/stringMethods.js
+++ exercise/stringMethods.js
@@ -10,3 +10,17 @@ const lastname = userName.slice(userName.lastIndexOf(' ') + 1, userName.length);
 
 console.log(firstname);
 console.log(lastname);
+
+
+
+
+/*
+    Exercise 2
+    ----------
+    Замаскувати все, крім останніх чотирьох символів
+    const card = '2342834503458353' ----> ************8353
+ */
+
+const card = '2342834503458353';
+console.log(card.slice(-4).padStart(16, '*'));
+
