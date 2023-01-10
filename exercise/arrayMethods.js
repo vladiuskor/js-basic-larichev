506d9d1d4011aee93f2ba1216dbecec96206bb08
Vlad Korobko <max.vlad.kor@gmail.com>
2023-01-10 13:12:10 +0200

    Added array methods second exercise solution

--- exercise/arrayMethods.js
+++ exercise/arrayMethods.js
@@ -1,4 +1,6 @@
 /*
+    Exercise 1
+    ----------
     Наявний масив зміни цін prices, де всередині 1-ий елемент масиву є ціною у момент Х,
     2-ий - ціною в момент У.
     Необхідно перетворити данні у масив, де будуть показані тільки позитивні зміни цін: [100, 150]
@@ -10,8 +12,26 @@ const prices = [
     [200, 350]
 ];
 
-let result = prices.map(elem => {
+let resultExercise1 = prices.map(elem => {
    return elem[1] - elem[0]
 }).filter(elem => elem > 0)
 
-console.log(result);
+console.log('Результат першого завдання:', resultExercise1);
+
+
+/*
+    Exercise 2
+    ----------
+    Знайти середнє значення послідовності чисел за допомогою reduce
+ */
+
+const arr = [1, 4, 4, 10];
+const average = arr.reduce((acc, value, index) => {
+    if(index !== arr.length - 1) {
+        return acc + value;
+    } else {
+        return (acc + value) / arr.length;
+    }
+}, 0);
+
+console.log('Результат другого завдання:', average);
