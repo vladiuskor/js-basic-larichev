a8b8ef22ad900f5f8f63b7d53ae8b4f2b3207d45
Vlad Korobko <max.vlad.kor@gmail.com>
2023-02-14 17:47:00 +0200

    Added popup

--- scripts/app.js
+++ scripts/app.js
@@ -78,7 +78,7 @@ function rerenderContent(activeHabbit) {
         elem.classList.add('habbit');
         elem.innerHTML = `<div class="habbit__day">Day ${Number(index)+ 1}</div>
                     <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
-                    <button class="habbit__delete">
+                    <button class="habbit__delete" onclick="deleteDay(${index})">
                         <img src="./images/delete.svg" alt="Delete day ${Number(index) + 1}">
                     </button>`
         page.content.daysContainer.appendChild(elem);
@@ -123,6 +123,21 @@ function addDays(event) {
     });
 
     form['comment'].value = '';
+    rerender(globalActiveHabbitId);
+    saveData();
+}
+
+function deleteDay(index) {
+    habbits = habbits.map(habbit => {
+        if(habbit.id === globalActiveHabbitId) {
+            habbit.days.splice(index, 1);
+            return {
+                ...habbit,
+                days: habbit.days
+            }
+        }
+        return habbit;
+    });
     rerender(globalActiveHabbitId);
     saveData();
 }
